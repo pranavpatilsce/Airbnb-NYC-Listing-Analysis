7a3268360a7853665cc263c7c40b636a89a611c3
pranavpatilsce <ompranav97@gmail.com>
2019-11-15 21:00:57 -0800

    readied the flask to serve

--- service/app.py
+++ service/app.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify, request, make_response
 # from sklearn.externals import joblib
 from flask_restplus import fields, Resource, Api
 
-app = Flask(__name__)
+#app = Flask(__name__)
+
+app = flask.Flask("__main__")
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return flask.render_template("index.html", token="airbnbpriceprediction")
 
+app.run(debug=True)
